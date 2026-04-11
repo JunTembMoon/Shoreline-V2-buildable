b6738f3d195c09275f95fce9e37207192945f9a0
LainMC <ssangnamja.com@gmail.com>
2026-04-11 17:33:16 +0900

    fixing bugs ++

--- src/main/java/net/shoreline/client/mixin/gui/screen/MixinScreen.java
+++ src/main/java/net/shoreline/client/mixin/gui/screen/MixinScreen.java
@@ -13,6 +13,7 @@ import net.minecraft.client.gui.screen.world.SelectWorldScreen;
 import net.shoreline.client.impl.module.client.ClickGuiModule;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
